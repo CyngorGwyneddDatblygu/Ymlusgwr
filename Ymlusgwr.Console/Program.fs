a809a85d80b139ba85ac0bc932ada8b62017221a
Ellyll <cawsllyffant-git@yahoo.com>
2018-08-11 12:03:42 +0100

    Ychwanegu statws i'r rhestr cysylltau wedi torri

--- Ymlusgwr.Console/Program.fs
+++ Ymlusgwr.Console/Program.fs
@@ -17,21 +17,27 @@ let main argv =
             for kv in tudalennau do
                 match kv.Value.Statws with
                 | Some statws when statws = HttpStatusCode.OK -> ()
-                | _ -> yield kv.Value.UriTudalen
+                | _ -> yield kv.Value.UriTudalen, kv.Value.Statws
         ]
-        |> List.map (fun uri ->
+        |> List.map (fun (uri, statws) ->
                 uri.ToString(),
-                [
-                    for kv in tudalennau do
-                        if kv.Value.Cysylltau |> List.contains uri then
-                            yield kv.Key.ToString()
-                ]
+                (
+                    (match statws with
+                    | Some httpStatusCode -> (httpStatusCode.ToString())
+                    | _ -> ""),
+    
+                    [
+                        for kv in tudalennau do
+                            if kv.Value.Cysylltau |> List.contains uri then
+                                yield kv.Key.ToString()
+                    ]
+                )
             )
         |> Map.ofList
     
     cysylltauWediTorri
-    |> Map.iter (fun cyswlltWediTorri tudalennauGydarCyswllt ->
-        printfn "%s" cyswlltWediTorri
+    |> Map.iter (fun cyswlltWediTorri (statws, tudalennauGydarCyswllt) ->
+        printfn "%s\t%s" cyswlltWediTorri statws
         tudalennauGydarCyswllt |> List.iter(fun tud -> printfn "\t%s" tud))    
     
     0
