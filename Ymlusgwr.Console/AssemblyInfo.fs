963783f0d1027d0910270d7961cbba7bb4576c12
Ellyll <cawsllyffant-git@yahoo.com>
2018-08-25 11:10:39 +0100

    Ychwanegu / defnyddio CommandLineParser

---
+++ Ymlusgwr.Console/AssemblyInfo.fs
@@ -0,0 +1,33 @@
+
+namespace Ymlusgwr.Console.AssemblyInfo
+
+open System
+open System.Reflection
+
+[<assembly: AssemblyProduct("Ymlusgwr")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyCompany("Ellyll")>]
+[<assembly: AssemblyCopyright("Copyright (c) Ellyll. All rights reserved.")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyMetadata("Serviceable", "True")>]
+
+//[<assembly: AssemblyFileVersion("3.14.40629.0536")>]
+[<assembly: AssemblyFileVersion("1.0.0.1")>]
+
+// On official build, attribute AssemblyInformationalVersionAttribute is added as well
+// with its value equal to the hash of the last commit to the git branch.
+// e.g.: [assembly: AssemblyInformationalVersionAttribute("4392c9835a38c27516fc0cd7bad7bccdcaeab161")]
+
+[<assembly: CLSCompliant(false)>]
+
+
+//[assembly: AssemblyInformationalVersionAttribute("b690ccd4070087b77149d5e95e1730cbdeb168ab")]
+
+
+//[assembly: AssemblyVersion("3.14.1.10")]
+[<assembly: AssemblyVersion("1.0.0.1")>]
+
+do
+    ()
--- Ymlusgwr.Console/Program.fs
+++ Ymlusgwr.Console/Program.fs
@@ -1,14 +1,27 @@
 ﻿module Program
 
+open System.Diagnostics
 open System.Net
+open CommandLine
 open Ymlusgo
 open Cofnodi
 
-[<EntryPoint>]
-let main argv =
-    let url = "https://oriel.madarch.org"
+type Options = {
+    [<Value(0, MetaName = "url", HelpText = "Y URL i ymlusgo", Required = true)>]
+    Url : string
+}
+
+type CanlyniadRhedeg =
+    | Llwyddiant = 0
+    | Methiant = 1
+
+
+let rhedeg dewisiadau = 
+    let url = dewisiadau.Url
+
+    let stopwatch = Stopwatch.StartNew()
     
-    //gosodMathauICofnodi [ Dadfygio ]
+    printfn "Ymlusgo %s" url
    
     let tudalennau = ymlusgo url
     
@@ -39,5 +52,26 @@ let main argv =
     |> Map.iter (fun cyswlltWediTorri (statws, tudalennauGydarCyswllt) ->
         printfn "%s\t%s" cyswlltWediTorri statws
         tudalennauGydarCyswllt |> List.iter(fun tud -> printfn "\t%s" tud))    
+
+    stopwatch.Stop()
+    printfn "Wedi gorffen am %s, cymerwyd %d eiliad." (System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")) (stopwatch.ElapsedMilliseconds/(int64 1000))
+    CanlyniadRhedeg.Llwyddiant
+
+let methu gwallau =
+    //printfn "Methu: Errors = %A" errors
+    CanlyniadRhedeg.Methiant
+
+[<EntryPoint>]
+let main argv =
+    //let url = "https://oriel.madarch.org"
     
-    0
+    //gosodMathauICofnodi [ Dadfygio ]
+    
+    let result = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+    let canlyniadRheged =
+        match result with
+        | :? Parsed<Options> as parsed -> rhedeg parsed.Value
+        | :? NotParsed<Options> as notParsed -> methu notParsed.Errors
+        | _ -> failwith "Gwall wrth dosrannu paramedrau"
+
+    int canlyniadRheged
